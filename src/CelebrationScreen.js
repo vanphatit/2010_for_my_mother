cd0f1ad77ce89e6154ff04b66f6df5a2db41143e
Van Phat <vanphat15it@gmail.com>
2024-10-20 10:25:17 +0700

    addsound

--- src/CelebrationScreen.js
+++ src/CelebrationScreen.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { TypeAnimation } from "react-type-animation";
 import './CelebrationScreen.css';
 
 const CelebrationScreen = () => {
+    const audioRef = useRef(null);
+  
+    // useEffect để tự động phát nhạc khi component được render
+    useEffect(() => {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+    }, []);
   return (
     <div className="celebration-screen">
+        <audio ref={audioRef} src="/formymother.mp3" loop />
       <TypeAnimation
         sequence={[
-          "Chúc mừng Ngày phụ nữ Việt Nam 20/10! 🎉", 1500,
-          "Mẹ là người phụ nữ tuyệt vời nhất! 💖", 1500,
-          "Con luôn biết ơn vì tình yêu thương vô điều kiện của mẹ! 🌹", 1500,
-          "Mong mẹ luôn khỏe mạnh và hạnh phúc mỗi ngày! 🌷", 1500,
-          "Mẹ là nguồn động lực lớn nhất của con! 💪", 1500
+          "Chúc mừng Ngày phụ nữ Việt Nam 20/10! 🎉", 3500,
+          "Mẹ là người phụ nữ tuyệt vời nhất! 💖", 3500,
+          "Con luôn biết ơn vì tình yêu thương vô điều kiện của mẹ! 🌹", 3500,
+          "Mong mẹ luôn khỏe mạnh và hạnh phúc mỗi ngày! 🌷", 3500,
+          "Mẹ là nguồn động lực lớn nhất của con! 💪", 3500,
+          "Nha mẹ! 💕", 3500,
         ]}
         wrapper="h1"
         speed={50}
